44a38602eb27d7bf28ad9364d9080a6edf276e8d
Gahlot Kapil <gahlotkapil98@gmail.com>
2024-10-20 01:16:04 +0530

    removed PT

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,7 +1,7 @@
 
 
 export default function handler(req, res) {
-  const instagramAuthUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=https://instamate-git-main-kapilgahlot1998s-projects.vercel.app/api/auth/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish`;
+  const instagramAuthUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=6833791376744849&redirect_uri=https://instamate-git-main-kapilgahlot1998s-projects.vercel.app/api/auth/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish`;
   
   res.redirect(instagramAuthUrl);
 }
